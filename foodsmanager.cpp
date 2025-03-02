dee16d95b7bcc0290af94201786a6383d60aa879
R3furbished <migas.2.3@outlook.pt>
2025-03-02 23:10:54 +0000

    Skeleton for foodsManager class

--- foodsmanager.cpp
+++ foodsmanager.cpp
@@ -3,3 +3,21 @@
 FoodsManager::FoodsManager(QObject *parent)
     : QObject{parent}
 {}
+
+QList<FoodItem *> FoodsManager::foods() const
+{
+    return m_foods;
+}
+
+void FoodsManager::setFoods(const QList<FoodItem *> &newFoods)
+{
+    if (m_foods == newFoods)
+        return;
+    m_foods = newFoods;
+    emit foodsChanged();
+}
+
+void FoodsManager::resetFoods()
+{
+    setFoods({}); // TODO: Adapt to use your actual default value
+}
--- foodsmanager.h
+++ foodsmanager.h
@@ -10,10 +10,19 @@ class FoodsManager : public QObject
 public:
     explicit FoodsManager(QObject *parent = nullptr);
 
+    QList<FoodItem *> foods() const;
+    void setFoods(const QList<FoodItem *> &newFoods);
+    void resetFoods();
+    void addFoodItem();// This has to take info from Qt side and pass it into the FoodItem constructor
+
+
 signals:
+
+    void foodsChanged();
 
 private:
     QList<FoodItem *> m_foods;
+    Q_PROPERTY(QList<FoodItem *> foods READ foods WRITE setFoods RESET resetFoods NOTIFY foodsChanged FINAL)
 };
 
 #endif // FOODSMANAGER_H
